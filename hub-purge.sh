b666f9bf2ee5a4c4381fe7678e1a5bcee5bf0e7c
mpyw <ryosuke_i_628@yahoo.co.jp>
2017-05-21 02:36:47 +0900

    Initial commit

---
+++ hub-purge.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Validate arguments
+if [ $# -lt 1 ]; then
+    >&2 echo "Usage: $0 <username/repo_name | full_url> ...\n"
+    exit 1
+fi
+
+# Iterate over arguments
+for url in "$@"; do
+
+    # Generate GitHub repository URL
+    url=`sed -e 's@^[A-Za-z0-9-][A-Za-z0-9-]*/[A-Za-z0-9-][A-Za-z0-9-]*$@https://github.com/&@' <<< "$url"`
+
+    # Fetch README page
+    echo "Fetching: $url"
+    curl -sL "$url" \
+    | grep -oE '<img src="https?://camo.githubusercontent.com/[^"]+' \
+    | sed -e 's/<img src="//' \
+    | while read url; do
+
+        # Purge resources
+        echo "Purging: $url"
+        curl -sX PURGE "$url" >/dev/null &
+
+    done &
+done
+
+# Wait all jobs done
+wait
